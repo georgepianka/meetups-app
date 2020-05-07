cc157f64e1f0f778c974e16d66304e7f7c0a4718
NotoriousCottonball <gpianka1@gmail.com>
2020-05-07 00:04:44 -0700

    Setup update meetup form controller event

--- both/controllers/myMeetups.js
+++ both/controllers/myMeetups.js
@@ -15,7 +15,39 @@ MyMeetupsController.helpers({
 });
 
 MyMeetupsController.events({
-  'click [data-action=doSomething]': function (event, template) {
+  'submit .update-meetup-form': function(event) {
     event.preventDefault();
+    let title = event.target.title.value;
+    let email = event.target.email.value;
+    let topics = event.target.topics.value;
+    let type = event.target.type.value;
+    let address = event.target.address.value;
+    let city = event.target.city.value;
+    let state = event.target.state.value;
+    let zipcode = event.target.zipcode.value;
+    let meetupDate = event.target.meetup_date.value;
+    let id = event.target.id.value;
+
+    let params = {
+      title: title,
+      email: email,
+      topics: topics,
+      type: type,
+      address: address,
+      city: city,
+      state: state,
+      zipcode: zipcode,
+      user: Meteor.userId(),
+      username: Meteor.user().username,
+      meetupDate: meetupDate,
+      createdAt: new Date()
+    }
+
+    //insert
+    Meteor.call('addMeetup', params);
+
+    toastr.success('Meetup Posted!');
+
+    Router.go('/meetups');
   }
 });
