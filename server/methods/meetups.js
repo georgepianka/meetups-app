99f0b457c914c347fe76a5af4a042b54a6b5dbc9
NotoriousCottonball <gpianka1@gmail.com>
2020-05-07 00:19:16 -0700

    Write updateMeetup method on server

--- server/methods/meetups.js
+++ server/methods/meetups.js
@@ -1,5 +1,11 @@
 Meteor.methods({
   'addMeetup': function(params) {
     Meetups.insert(params);
+  },
+  'updateMeetup': (id, params) => {
+    Meetups.update(
+       {_id: id},
+       {$set:params}
+     )
   }
 });
